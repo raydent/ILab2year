27c545f26b26b429b1e5e870641c66c5a6a3585d
raydent <benisgachi@gmail.com>
2019-11-08 21:55:01 +0300

    some methods are const now

--- TCS/TIS.hpp
+++ TCS/TIS.hpp
@@ -20,11 +20,11 @@ struct vertex2D_t {
 template <typename T>
 struct polygon_t {
     std::list<vertex2D_t<T>> list;
-    polygon_t(){;}
-    polygon_t(vertex2D_t<T> A_, vertex2D_t<T> B_, vertex2D_t<T> C_) {list.push_front(A_); list.push_front(B_); list.push_front(C_);}
+    polygon_t(){}
+    explicit polygon_t(vertex2D_t<T> A_, vertex2D_t<T> B_, vertex2D_t<T> C_) {list.push_front(A_); list.push_front(B_); list.push_front(C_);}
     void push(int position, vertex2D_t<T>& elem);
-    double calc_square();
-    void print();
+    const double calc_square();
+    const void print();
 };
 
 template <typename T>
@@ -32,28 +32,26 @@ struct vec2D_t {
     T x, y;
     vertex2D_t<T> A, B;
     double k;
-    vec2D_t(vertex2D_t<T> A_, vertex2D_t<T> B_) : x(B_.x - A_.x), y(B_.y - A_.y), A(A_), B(B_) {x == 0 ? k = 0 : k = y / x;}
-    double cross_product(vec2D_t<T> rhs);
-    double cross_product(vertex2D_t<T> rhs);
-    bool findCrossing(vec2D_t<T> rhs, vertex2D_t<T>& ret1);
-    void clipPolygon(vertex2D_t<T> C, polygon_t<T>& polygon);
+    explicit vec2D_t(vertex2D_t<T> A_, vertex2D_t<T> B_) : x(B_.x - A_.x), y(B_.y - A_.y), A(A_), B(B_) {x == 0 ? k = 0 : k = y / x;}
+    const double cross_product(vec2D_t<T> rhs);
+    const double cross_product(vertex2D_t<T> rhs);
+    const bool findCrossing(vec2D_t<T> rhs, vertex2D_t<T>& ret1);
+    const void clipPolygon(vertex2D_t<T> C, polygon_t<T>& polygon);
     vec2D_t<T> makeVec(vertex2D_t<T> A_, vertex2D_t<T> B_);
-    bool rightside(vertex2D_t<T> C, vertex2D_t<T> rhs);
-    void remove_wrong_points_from_polygon(polygon_t<T>& polygon, vertex2D_t<T> C);
-    void print();
+    const bool rightside(vertex2D_t<T> C, vertex2D_t<T> rhs);
+    const void remove_wrong_points_from_polygon(polygon_t<T>& polygon, vertex2D_t<T> C);
+    const void print();
 };
 
 template <typename T>
 struct triangle_t {
     vertex2D_t<T> A, B, C;
     vec2D_t<T> AB, BC, CA;
-    triangle_t(vertex2D_t<T> A_, vertex2D_t<T> B_, vertex2D_t<T> C_): A(A_), B(B_), C(C_), AB{A_, B_}, BC{B_, C_}, CA{C_, A_} {}
-    double intersection_area(const triangle_t<T> &rhs);
-    void print();
+    explicit triangle_t(vertex2D_t<T> A_, vertex2D_t<T> B_, vertex2D_t<T> C_): A(A_), B(B_), C(C_), AB{A_, B_}, BC{B_, C_}, CA{C_, A_} {}
+    const double intersection_area(const triangle_t<T> &rhs);
+    const void print();
 };
 
-template <typename T>
-double calc_square(std::list<vertex2D_t<T>> polygon);
 
 template <typename T>
 bool operator == (const vertex2D_t<T> &c1, const vertex2D_t<T> &c2) {
@@ -77,18 +75,18 @@ vec2D_t<T> vec2D_t<T>::makeVec(vertex2D_t<T> A_, vertex2D_t<T> B_){
 }
 
 template <typename T>
-double vec2D_t<T>::cross_product(vec2D_t<T> rhs){
+const double vec2D_t<T>::cross_product(vec2D_t<T> rhs){
     return ((y * rhs.x) - (x * rhs.y));
 }
 
 template <typename T>
-double vec2D_t<T>::cross_product(vertex2D_t<T> rhs){
+const double vec2D_t<T>::cross_product(vertex2D_t<T> rhs){
     vec2D_t BC {B, rhs};
     return cross_product(BC);
 }
 
 template <typename T>
-void vec2D_t<T>::clipPolygon(vertex2D_t<T> C, polygon_t<T>& polygon){
+const void vec2D_t<T>::clipPolygon(vertex2D_t<T> C, polygon_t<T>& polygon){
     std::vector<vertex2D_t<T>> pointarr;
     std::vector<int> arr1;
     vertex2D_t<T> tempvert;
@@ -97,7 +95,6 @@ void vec2D_t<T>::clipPolygon(vertex2D_t<T> C, polygon_t<T>& polygon){
     int elempos = 0;
     auto it = polygon.list.begin();
     auto it2 = polygon.list.begin();
-    //assert(it2 != polygon.list.end());
     it2++;
     for(; it2 != polygon.list.end(); it++, it2++){
         elempos++;
@@ -122,7 +119,7 @@ void vec2D_t<T>::clipPolygon(vertex2D_t<T> C, polygon_t<T>& polygon){
 }
 
 template <typename T>
-double triangle_t<T>::intersection_area(const triangle_t<T> &rhs){
+const double triangle_t<T>::intersection_area(const triangle_t<T> &rhs){
     polygon_t<T> polygon(rhs.A, rhs.B, rhs.C);
     AB.clipPolygon(C, polygon);
     BC.clipPolygon(A, polygon);
@@ -132,7 +129,7 @@ double triangle_t<T>::intersection_area(const triangle_t<T> &rhs){
 }
 
 template <typename T>
-bool vec2D_t<T>::rightside(vertex2D_t<T> C, vertex2D_t<T> rhs){
+const bool vec2D_t<T>::rightside(vertex2D_t<T> C, vertex2D_t<T> rhs){
     double crossC = cross_product(C);
     double crossRhs = cross_product(rhs);
     if (crossRhs == 0)
@@ -143,7 +140,7 @@ bool vec2D_t<T>::rightside(vertex2D_t<T> C, vertex2D_t<T> rhs){
 }
 
 template <typename T>
-bool vec2D_t<T>::findCrossing(vec2D_t<T> rhs, vertex2D_t<T>& ret1){
+const bool vec2D_t<T>::findCrossing(vec2D_t<T> rhs, vertex2D_t<T>& ret1){
     vertex2D_t<T> trash{0, 0};
     if (k == rhs.k && ((y == rhs.y) || (x == rhs.x))){
         return false;
@@ -235,17 +232,17 @@ bool vec2D_t<T>::findCrossing(vec2D_t<T> rhs, vertex2D_t<T>& ret1){
 }
 
 template <typename T>
-void vec2D_t<T>::print(){
+const void vec2D_t<T>::print(){
     std::cout << x << " " << y << " " << A.x << " " << A.y << " " << B.x << " " << B.y << " " << k << "\n";
 }
 
 template <typename T>
-void triangle_t<T>::print(){
+const void triangle_t<T>::print(){
     std::cout << A.x << " " << A.y << " " << B.x << " " << B.y << " " << C.x << " " << C.y << " " << AB.x << " " << AB.y << " " << AB.k << "\n";
 }
 
 template <typename T>
-double polygon_t<T>::calc_square(){
+const double polygon_t<T>::calc_square(){
     double square = 0;
     auto it1 = list.begin();
     auto it2 = list.begin();
@@ -279,7 +276,7 @@ void polygon_t<T>::push(int elempos, vertex2D_t<T>& elem){
 }
 
 template <typename T>
-void vec2D_t<T>::remove_wrong_points_from_polygon(polygon_t<T>& polygon, vertex2D_t<T> C){
+const void vec2D_t<T>::remove_wrong_points_from_polygon(polygon_t<T>& polygon, vertex2D_t<T> C){
     for(auto it = polygon.list.begin(); it != polygon.list.end(); it++){
         if (rightside(C, *it) != 1){
             auto tempit = it;
@@ -291,7 +288,7 @@ void vec2D_t<T>::remove_wrong_points_from_polygon(polygon_t<T>& polygon, vertex2
 }
 
 template <typename T>
-void polygon_t<T>::print(){
+const void polygon_t<T>::print(){
     printf("vertexes starting\n");
     for(auto it = list.begin(), ite = list.end(); it != ite; ++it){
         printf("x = %f y = %f\n", it -> x, it -> y);
